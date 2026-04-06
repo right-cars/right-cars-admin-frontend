72b665e959daafb7c410809f87d478ecf48cdf36
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2026-04-06 07:52:30 +0200

    fix

--- src/app/edit/[id]/temporary.tsx
+++ src/app/edit/[id]/temporary.tsx
@@ -62,6 +62,7 @@ export const temporary = [
               { id: nanoid(), value: "Seat", text: "Seat" },
               { id: nanoid(), value: "Mahindra", text: "Mahindra" },
               { id: nanoid(), value: "MG", text: "MG" },
+              { id: nanoid(), value: "Jinbei", text: "Jinbei" },
         ] },
       { id: nanoid(), label: "km", name:"mileageInKm", value: "", mask: true },
       { id: nanoid(), label: "model", name: "model", value: "" },
