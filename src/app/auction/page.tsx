86156d8c52d58c8ec296438534cfcf781d62a398
BogdanLyamzin <bogdan.lyamzin.d@gmail.com>
2026-04-06 07:59:58 +0200

    fix

--- src/app/auction/page.tsx
+++ src/app/auction/page.tsx
@@ -4,15 +4,15 @@ import AuctionToolbar from "@/components/modules/ToolBar/AuctionToolbar";
 // import StatusTabs from "@/components/views/Home/StatusTabs";
 import AuctionStatusTabs from "@/components/views/Home/AuctionStatusTabs";
 
-import {getAuctions} from "@/api/auctions";
+// import {getAuctions} from "@/api/auctions";
 
 export default async function Action() {
-    const auctions = await getAuctions();
+    // const auctions = await getAuctions();
 
   return (
       <Container>
           <AuctionToolbar title="auction" />
-          <AuctionStatusTabs auctions={auctions}/>
+          <AuctionStatusTabs auctions={[]}/>
       </Container>
   )
 }
